822176c81eb400dd1eedc3e9dba0e74407c83e6b
Luiz Henrique <7henrique18@gmail.com>
2021-10-23 18:11:51 -0300

    feat: show message in real time

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,73 +1,74 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
+import io from "socket.io-client";
 
 import styles from "./styles.module.scss";
 import logoImg from "../../assets/logo.svg";
 
+type Message = {
+  created_at: string;
+  id: string;
+  text: string;
+  user_id: string;
+  user: {
+    avatar_url: string;
+    bio: string;
+    github_id: string;
+    id: string;
+    location: string;
+    login: string;
+    name: string;
+    url: string;
+  };
+};
+
+const messagesQueue: Message[] = [];
+
+const socket = io("http://localhost:4000");
+
+socket.on("new_message", (newMessage: Message) => {
+  messagesQueue.push(newMessage);
+});
+
 export function MessageList() {
+  const [messages, setMessages] = useState<Message[]>([]);
+
   useEffect(() => {
-    const getMessages = async () => {
-      const { data } = await api.get("/messages/last3/");
-      console.log(data);
-    };
-    getMessages();
+    setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setMessages((prevState) =>
+          [messagesQueue[0], prevState[0], prevState[1]].filter(Boolean)
+        );
+
+        messagesQueue.shift();
+      }
+    }, 3000);
+  }, []);
+
+  useEffect(() => {
+    api.get<Message[]>("messages/last3").then((response) => {
+      setMessages(response.data);
+    });
   }, []);
 
   return (
     <div className={styles.messageListWrapper}>
       <img src={logoImg} alt="DoWhile 2021" />
+
       <ul className={styles.messageList}>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae
-            laudantium neque consectetur molestias aliquam ab reprehenderit,
-            delectus quod? Modi laboriosam eum magni molestiae non beatae quam
-            vel delectus ipsa suscipit?
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img
-                src="https://github.com/lui7henrique.png"
-                alt="Luiz Henrique"
-              />
-            </div>
-            <span>Luiz Henrique</span>
-          </div>
-        </li>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae
-            laudantium neque consectetur molestias aliquam ab reprehenderit,
-            delectus quod? Modi laboriosam eum magni molestiae non beatae quam
-            vel delectus ipsa suscipit?
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img
-                src="https://github.com/lui7henrique.png"
-                alt="Luiz Henrique"
-              />
-            </div>
-            <span>Luiz Henrique</span>
-          </div>
-        </li>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae
-            laudantium neque consectetur molestias aliquam ab reprehenderit,
-            delectus quod? Modi laboriosam eum magni molestiae non beatae quam
-            vel delectus ipsa suscipit?
-          </p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img
-                src="https://github.com/lui7henrique.png"
-                alt="Luiz Henrique"
-              />
-            </div>
-            <span>Luiz Henrique</span>
-          </div>
-        </li>
+        {messages.map((message) => {
+          return (
+            <li key={message.id} className={styles.message}>
+              <p className={styles.messageContent}>{message.text}</p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>
+                  <img src={message.user.avatar_url} alt={message.user.name} />
+                </div>
+                <span>{message.user.name}</span>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
